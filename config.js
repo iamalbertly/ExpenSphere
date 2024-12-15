bec91479763c70cc04ea5cf8ca91982469fc1841
iamalbertly <116843037+iamalbertly@users.noreply.github.com>
2024-12-15 22:34:19 +0300

    fix origin issues

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
-const config = {
-    clientId: '35324134118-oit317sdim38b1h0pbmje25j8dfs10jo.apps.googleusercontent.com',
-    clientSecret: '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'
+export default {
+    "clientId": "1072662115353-qcn0bqo6l8r2r7qv0a9iqhdt0jmhqj5g.apps.googleusercontent.com",
+    "clientSecret": "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",
+    "apiKey": "AIzaSyBOukcVnqFX_ZYRfOfnbVr5V7QqLtg9Hn0"
 };
-export default config;
