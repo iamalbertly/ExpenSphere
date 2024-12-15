8fe89c2426bf9b97fed98d820545b630ca0bf937
iamalbertly <116843037+iamalbertly@users.noreply.github.com>
2024-12-15 20:31:23 +0300

    added saved transactions

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    clientId: '35324134134118-oit317sdim38b1h0pbmje25j8dfs10jo.apps.googleusercontent.com',
+    clientId: '35324134118-oit317sdim38b1h0pbmje25j8dfs10jo.apps.googleusercontent.com',
     clientSecret: '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'
 };
 export default config;
